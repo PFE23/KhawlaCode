ba7f886d72cd37dcbf8ff1e44eb390f0eb219ef3
R. ALOUI <aloui.radhouane@gmail.com>
2023-03-29 11:29:03 +0100

    Connect to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 #include <Arduino.h>
 #include "Button.hpp"
 #include "Cycle.hpp"
+#include <WiFi.h>
 
 
 #define PIN_START_BUTTON  23
 #define PIN_STOP_BUTTON   22
+#define ssid              "Redmi Note 11"
+#define password          "isetbeja"
+#define WAITTIME          2000
 
 Button start(PIN_START_BUTTON);
 Button stop(PIN_STOP_BUTTON);
+
+time_t startTime;
 
 void setup(){
   Serial.begin(115200);
@@ -18,11 +24,24 @@ void setup(){
   stop.begin(&stopCycle);
   cycles->clear();
   Serial.printf("The size of stats is %d bytes\n", sizeof(stats_t));
+  // Connecting to WiFi
+  Serial.print("Connecting to WiFi ");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(2000);
+  }
+  Serial.print(WiFi.localIP());
+
+  startTime = millis();
   Serial.printf("start;stop;dur;shortD;longD;work;sleep;size\n");
   //Serial.printf("start;start;stop;stop;dur;dur;shortD;shortD;longD;longD;work;work;sleep;sleep;size\n");
 }
 
 void loop(){
-  printStats2();
-  delay(1000);
+  time_t now = millis();
+  if (now - startTime > WAITTIME){
+    printStats2();
+    startTime = now;
+  }
 }
